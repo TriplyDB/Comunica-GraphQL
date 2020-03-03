788d9d042f410cfd7b19adee3ef89edc06617ed3
Thomas de Groot <thomas.de.groot@triply.cc>
2020-03-03 11:58:44 +0100

    Working fragments example

--- configurer/src/index.ts
+++ configurer/src/index.ts
@@ -1,22 +1,45 @@
 import * as superagent from "superagent";
 
+// const endpoint =
+//   "https://api.nightly.triply.cc/datasets/ysg/bob/services/bob/sparql";
+// const endpointType = "SPARQL";
 const endpoint =
-  "https://api.nightly.triply.cc/datasets/ysg/bob/services/bob/sparql";
-const endpointType = "SPARQL";
+  "https://api.nightly.triply.cc/datasets/ysg/bob/fragments";
+const endpointType = "fragments";
 const typeDefs = `
   type Query  {
-      mes: me
+      mes: [me],
+      hero: [hero]
   }
+
   type me {
     name: [String]
+  }
+
+  type hero {
+    name: [String]
+    id2: [String]
+    friends: [hero]
   }
   `;
 
 const context = {
   "@context": {
     me: "http://example.org/me",
+    id2: "http://example.org/id",
+    mes: "http://example.org/me",
     name: "http://example.org/name",
-    mes: "http://example.org/me"
+    hero: "http://example.org/hero",
+    human: "http://example.org/human",
+    height: "http://example.org/height",
+    friends: "http://example.org/friends",
+    episode: "http://example.org/episode",
+    primaryFunction: "http://example.org/primaryFunction",
+    JEDI: "http://example.org/types/Jedi",
+    Droid: "http://example.org/types/Droid",
+    Human: "http://example.org/types/Human",
+    appearsIn: "http://example.org/appearsIn",
+    EMPIRE: "http://example.org/types/Empire"
   }
 };
 
