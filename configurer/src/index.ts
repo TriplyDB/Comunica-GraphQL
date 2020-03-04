8bbfe3b8b1ce4a55e2e6c091ee46cc05f35eda34
Thomas de Groot <thomas.de.groot@triply.cc>
2020-03-04 15:44:04 +0100

    Added in SPARQL query printer

--- configurer/src/index.ts
+++ configurer/src/index.ts
@@ -7,11 +7,10 @@ const endpoint =
 //const endpointType = "fragments";
 const typeDefs = `
   type Query  {
-      brewedBy: [brewery],
-      regio: [buurt]
       getBag(bouwjaar: Int): [BAGpand]
       test(bouwjaar: Int): [BAGpand]
   }
+
 
   type BAGpand  {
     identificatiecode: [String]
@@ -22,66 +21,12 @@ const typeDefs = `
   type Label {
     label: [String]
   }
-
-  type buurt {
-    hasGeometry: [geometery],
-    sfWithin: [wijk],
-    label: [String],
-    regioCode: [String]
-  }
-
-  type geometery {
-    asWKT: [String]
-  }
-
-  type wijk {
-    hasGeometry: [geometery],
-    sfWithin: [gemeente],
-    label: [String],
-    regioCode: [String]
-  }
-
-  type gemeente {
-    hasGeometry: [geometery],
-    sfWithin: [land],
-    label: [String],
-    regioCode: [String]
-  }
-
-  type land {
-    hasGeometry: [geometery],
-    label: [String],
-    regioCode: [String]
-  }
-
-
-  type brewery {
-    address: [Address],
-    email: [String],
-    jaarproduktie: [String]
-  }
-  type Address {
-    addressLocality: [String],
-    postalCode: [String]
-    streetAddress: [String]
-  }
   `;
 
 const context = {
   "@context": {
-    brewedBy: "https://data.labs.kadaster.nl/dbeerpedia/dbeerpedia/vocab/brewedby",
-    address: "http://schema.org/address",
-    email: "http://schema.org/email",
-    opgericht: "https://data.labs.kadaster.nl/dbeerpedia/dbeerpedia/vocab/jaarproduktie",
-    addressLocality: "http://schema.org/addressLocality",
-    postalCode: "http://schema.org/postalCode",
-    streetAddress: "http://schema.org/streetAddress",
-    hasGeometry: "http://schema.org/addressLocality",
-    sfWithin: "http://schema.org/addressLocality",
     bouwjaar:"http://bag.basisregistraties.overheid.nl/def/bag#oorspronkelijkBouwjaar",
-    asWKT: "http://www.opengis.net/ont/geosparql#asWKT",
     label: "http://www.w3.org/2000/01/rdf-schema#label",
-    regioCode: "https://data.pldn.nl/cbs/wijken-buurten/def/cbs#regiocode",
     bagstatus: "http://bag.basisregistraties.overheid.nl/def/bag#status",
     identificatiecode:"http://bag.basisregistraties.overheid.nl/def/bag#identificatiecode",
     getBag: {"@reverse":"http://bag.basisregistraties.overheid.nl/def/bag#identificatiecode"},
--- documentation/queryTester/src/queries.ts
+++ documentation/queryTester/src/queries.ts
@@ -1,37 +1,31 @@
 const Converter = require("graphql-to-sparql").Converter;
-const algebra = new Converter().graphqlToSparqlAlgebra(
-  `{
-  getBag(bouwjaar:1880){
-    bagstatus {
-      label
-    }
-  }
-}`,
-  {
-    brewedBy:
-      "https://data.labs.kadaster.nl/dbeerpedia/dbeerpedia/vocab/brewedby",
-    address: "http://schema.org/address",
-    email: "http://schema.org/email",
-    bouwjaar:
-      "http://bag.basisregistraties.overheid.nl/def/bag#oorspronkelijkBouwjaar",
-    opgericht:
-      "https://data.labs.kadaster.nl/dbeerpedia/dbeerpedia/vocab/jaarproduktie",
-    addressLocality: "http://schema.org/addressLocality",
-    postalCode: "http://schema.org/postalCode",
-    streetAddress: "http://schema.org/streetAddress",
-    hasGeometry: "http://schema.org/addressLocality",
-    sfWithin: "http://schema.org/addressLocality",
-    asWKT: "http://www.opengis.net/ont/geosparql#asWKT",
-    label: "http://www.w3.org/2000/01/rdf-schema#label",
-    regioCode: "https://data.pldn.nl/cbs/wijken-buurten/def/cbs#regiocode",
-    bagstatus: "http://bag.basisregistraties.overheid.nl/def/bag#status",
-    identificatiecode:
-      "http://bag.basisregistraties.overheid.nl/def/bag#identificatiecode",
-    getBag: {
-      "@reverse":
-        "http://bag.basisregistraties.overheid.nl/def/bag#identificatiecode"
-    },
-    regio: "https://data.pldn.nl/cbs/wijken-buurten/def/dimension#regio"
-  }
-);
-console.info(JSON.stringify(algebra, null, 2));
+import { IVariablesDictionary } from "graphql-to-sparql";
+
+const context = {
+  bouwjaar:
+    "http://bag.basisregistraties.overheid.nl/def/bag#oorspronkelijkBouwjaar",
+  label: "http://www.w3.org/2000/01/rdf-schema#label",
+  bagstatus: "http://bag.basisregistraties.overheid.nl/def/bag#status",
+  identificatiecode:
+    "http://bag.basisregistraties.overheid.nl/def/bag#identificatiecode",
+  getBag: {
+    "@reverse":
+      "http://bag.basisregistraties.overheid.nl/def/bag#identificatiecode"
+  },
+  regio: "https://data.pldn.nl/cbs/wijken-buurten/def/dimension#regio",
+  test: "https://example.com/test"
+};
+
+const query = `
+test(bouwjaar:1700){bouwjaar}`;
+
+const variablesDict: IVariablesDictionary = {
+  episode: { kind: "EnumValue", value: "JEDI" }
+};
+
+async function test() {
+  const algebra = await new Converter().graphqlToSparqlAlgebra(query, context);
+  console.info(JSON.stringify(algebra, null, 2));
+}
+
+test();
