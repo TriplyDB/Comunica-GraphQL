7ed50a076f6b7289b1fdbd10a2695870deffb643
Thomas de Groot <thomas.de.groot@triply.cc>
2020-03-26 13:55:20 +0100

    #3971 handling of multiple key fields for extend queries now implemented.

--- comunica-api/src/middlewares/communicaExtendQuery.ts
+++ comunica-api/src/middlewares/communicaExtendQuery.ts
@@ -20,7 +20,7 @@ export async function printSPARQLQuery(
   console.info(toSparql(algebra));
 }
 
-function retrieveDatatype(value: any): RDF.NamedNode | "EnumValue" {
+function retrieveDatatype(value: any): RDF.NamedNode {
   const type = typeof value;
   if (type === "string") {
     return namedNode("http://www.w3.org/2001/XMLSchema#string");
@@ -69,33 +69,58 @@ export async function communicaExtendQuery(
   const sparqlAlgebraCommunica = await communicaQuery(query, context);
 
   // GENERATION OF VALUES CLAUSE
-  const predicate =
-    context["@context"]["" + Object.keys(variables.representations[0])[1]];
-  const variablesBinding = [variable("representations")];
+  const valuesBgp = Object.keys(variables.representations[0])
+    .map((key: string) => {
+      if (key !== "__typename") {
+        const predicate = context["@context"]["" + key];
+        return OperationFactory.createPattern(
+          DataFactory.variable("_entities"),
+          DataFactory.namedNode(predicate),
+          DataFactory.variable("representations_" + key)
+        );
+      }
+    })
+    .filter(x => x);
+  const variablesBinding = Object.keys(variables.representations[0])
+    .map((key: string) => {
+      if (key !== "__typename") {
+        return variable("representations_" + key);
+      }
+    })
+    .filter(x => x);
   const valueBindings = variables.representations.map(
     (variable: { [key: string]: any }) => {
-      const value = variable[Object.keys(variable)[1]];
-      if (context["@context"][value]) {
-        return { "?representations": namedNode(context["@context"][value]) };
-      } else {
-        const datatype = retrieveDatatype(value);
-        return { "?representations": literal("" + value, datatype) };
-      }
+      return Object.keys(variable)
+        .map((key: string) => {
+          if (key !== "__typename") {
+            const value = variable[key];
+            if (context["@context"][value]) {
+              return {
+                [`?representations_${key}`]: namedNode(
+                  context["@context"][value]
+                )
+              };
+            } else {
+              const datatype = retrieveDatatype(value);
+              return {
+                [`?representations_${key}`]: literal("" + value, datatype)
+              };
+            }
+          }
+        })
+        .filter(x => x)
+        .reduce((obj, item) => {
+          obj[Object.keys(item)[0]] = item[Object.keys(item)[0]];
+          return obj;
+        }, {});
     }
   );
-
   //CONTRUCTION OF COMBINED VALUES CLAUSE WITH SPARQL
   const sparqlAlgebra = OperationFactory.createProject(
     OperationFactory.createJoin(
       OperationFactory.createValues(variablesBinding, valueBindings),
       OperationFactory.createJoin(
-        OperationFactory.createBgp([
-          OperationFactory.createPattern(
-            DataFactory.variable("_entities"),
-            DataFactory.namedNode(predicate),
-            DataFactory.variable("representations")
-          )
-        ]),
+        OperationFactory.createBgp(valuesBgp),
         sparqlAlgebraCommunica.input
       )
     ),
